055a7e2dc3a3903532a13e774f2be639bb877c6e
benjamin farmer <viroben2001@gmail.com>
2022-08-03 11:36:45 -0700

    revised code

--- index.js
+++ index.js
@@ -1,72 +1,58 @@
-window.prompt('Do you head left or right?')
-let answer  = window.prompt('Do you head left or right?')
-var firstAnswer  = window.prompt('Do you head left or right?')
-console.log(firstAnswer)
-let firstAnswer  = window.prompt('Do you head left or right?')
-if(firstAnswer === 'left'){
-    let secondAnswer = window.prompt(`You come across a stray cat. 
-It scampers off down a small hole, just large enough for you to crawl through. 
-Do you follow it, or continue on your path?`)
+let Left = "You come across a stray cat. It scampers off down a small hole just large enough for you to crawl through. Do you follow it or continue on your path?"
+let Follow = "You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven."
+let Stay = "You live happily amongst the cats for the rest of your days."
+let Spreadtheword = "After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless."
+Continueon = "You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?"
+let Ladder = "After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home."
+let Staircase = "After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever."
+let Right = "You come across a snoring dragon. On the other side of him, you see a shiny chest of treasure. Another path would lead you away from the dragon altogether. Which do you take?"
+let Pastthedragon = "The dragon wakes up and sits upright. You only have a moment to respond, to stay or run:"
+var Stay1 = "You and the dragon have an uplifting conversation about local politics and become lifelong friends."
+let Run = "Quickly, you run back to the cave's entrance. Sheepish, you return home."
+let Awayfromthedragon = "After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do?"
+let Drawit = "You draw the flower, capturing only a fraction of its beauty with your quill. You bring the drawing home, somewhat disappointed, but over time, discover joy in sharing it with your friends and family, recounting the story of your days as a sorcerer with the aid of the sketch."
+let Pickit = "You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust."
 
-} else if(firstAnswer === 'right') {
-    let secondAnswer = window.prompt(`You come across a snoring dragon. 
-    On the other side of him, you see a shiny chest of treasure. Another path would 
-    lead you away from the dragon altogether. Which path do you take?`)
-}
-    
-    if ("Do you head to the left or to the right?"){
-         console.log(Left)
-            } 
-                    if  ( "You come Across AStray Cat It scampers off down a small hole just large enough for you to crawl through. Do you follow it or continue on your path?") {
-                                console.log("Follow")
-                                     } 
-                
-                                        if("You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.") {
-                                                console.log("Stay")
-                                                    }
-                                                        if ("You live happily amongst the cats for the rest of your days."){
-                                                            console.log("Spread the word")
-                                                        }
-                                                            
-                                                                if("After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless."){
-                                                                     console.log("Continue")
-                                                                     }
-                                                        
-                                                                        if ("You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?"){
-                                                                             console.log("Ladder")
-                                                                                }
-                                                
-                                                                 if ("After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home."){
-                                                                    console.log("Staircase")  
-                                                                     }
-                        
-                                                        if ("After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever."){
-                                                            console.log("Right")
-                                                         }
-                                
-                                       if ("You come across a snoring dragon. On the other side of him, you see a shiny chest of treasure. Another path would lead you away from the dragon altogether. Which do you take?"){
-                                        console.log("Past the dragon")
-                                       }
-                                                
-                    if ("The dragon wakes up and sits upright. You only have a moment to respond, to stay or run:"){
-                            console.log("V") 
-                                 }
-                                                                        
-                                        if ("You and the dragon have an uplifting conversation about local politics and become lifelong friends."){
-                                            console.log("Run")
-                                        }
-                                                                
-                                                            if ("Quickly, you run back to the cave's entrance. Sheepish, you return home."){
-                                                                console.log("Away from the dragon")
-                                                            }
-                                    
-                                                                    if ("After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do?"){
-                                                                       confirm.log("Draw it.") 
-                                                                    }
-                                
-                                                                        if ("You draw the flower, capturing only a fraction of its beauty with your quill. You bring the drawing home, somewhat disappointed, but over time, discover joy in sharing it with your friends and family, recounting the story of your days as a sorcerer with the aid of the sketch."){
-                                                                            console.log("Pick it.")
-                                                                        }
-                                                
-                                                    window.alert("You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust.") 
-    
+switch (anawer) {
+    case 0:
+      answer = "left";
+      break;
+    case 1:
+        answer = "followe";
+      break;
+    case 2:
+        answer = "stay";
+      break;
+    case 3:
+        answer= "Spreadtheword";
+      break;
+    case 4:
+        answer = "Continueon";
+      break;
+    case 5:
+        answer = "ladder";
+      break;
+    case 6:
+        answer = "Staircase";
+      break;
+    case 7:
+        answer = "right";
+     break;
+    case 8:
+        answer = "Pastthedragon"; 
+     break;
+    case 9:
+        answer= "Stay1";
+      break;
+     case 10:
+        answer = "run";
+       break;
+      case 11:
+        answer ="Awayfromthedragon";
+       break;
+      case 12:
+        answer = "Drawit";
+       break;
+     default:
+        answer = "Pickit";   
+     }
